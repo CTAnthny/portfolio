0b08209f1b4f9ceaaad1a4ed8b2ad52944a710df
Chris Anthony <ctanthny@gmail.com>
2020-04-30 11:29:57 -0400

    Resolve config errors and add lint

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Material UI Starter`,
-    description: `Kick off your next, great Gatsby project with this Material UI starter. This barebones starter ships with the main Gatsby and Material UI configuration files you might need.`,
-    author: `@dominicabela`,
+    title: `Chris Anthony Portfolio`,
+    description: `About me, resume and portfolio of work`,
+    author: `@ctanthny`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Chris Anthony Portfolio`,
+        short_name: `CA-Portfolio`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
